93f1c10f259d3a22fc21bf25e0517fbf0057a28a
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-13 15:08:39 +0900

    이벤트 연결
    버튼 클릭시 자기 자신의 OnClick을 호출

--- MyFirstProject/app/src/main/java/kr/ac/tukorea/myapplication/myfirstproject2018182031/MainActivity.java
+++ MyFirstProject/app/src/main/java/kr/ac/tukorea/myapplication/myfirstproject2018182031/MainActivity.java
@@ -3,16 +3,24 @@ package kr.ac.tukorea.myapplication.myfirstproject2018182031;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.pushMeButton);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
         TextView tv = findViewById(R.id.snumTextView);
-        tv.setText("Activity Created");
+        tv.setText("PushMe");
     }
 }
