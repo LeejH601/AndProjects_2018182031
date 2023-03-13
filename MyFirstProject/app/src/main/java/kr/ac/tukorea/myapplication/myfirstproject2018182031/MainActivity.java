7be3569929c0cf9a185e45c9fc2370e40e27b2b0
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-13 14:55:39 +0900

    MtFirst 생성 및 Activity.onCreate에서 텍스트 변

---
+++ MyFirstProject/app/src/main/java/kr/ac/tukorea/myapplication/myfirstproject2018182031/MainActivity.java
@@ -0,0 +1,18 @@
+package kr.ac.tukorea.myapplication.myfirstproject2018182031;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.snumTextView);
+        tv.setText("Activity Created");
+    }
+}
