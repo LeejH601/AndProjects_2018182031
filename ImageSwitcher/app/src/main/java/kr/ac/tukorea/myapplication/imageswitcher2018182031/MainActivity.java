4e94f6f74c87a41b572f8fc99690187896913387
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-14 14:11:15 +0900

    prev next 버튼 이벤트 로깅

--- ImageSwitcher/app/src/main/java/kr/ac/tukorea/myapplication/imageswitcher2018182031/MainActivity.java
+++ ImageSwitcher/app/src/main/java/kr/ac/tukorea/myapplication/imageswitcher2018182031/MainActivity.java
@@ -3,6 +3,9 @@ package kr.ac.tukorea.myapplication.imageswitcher2018182031;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        ImageView imgView = findViewById(R.id.MainSrcImg);
+        Log.d("MainActivity", "Prev Presssed");
+    }
+
+    public void onBtnNext(View view) {
+        Log.d("MainActivity", "Next Presssed");
     }
 }
