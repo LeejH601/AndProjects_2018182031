8c15a0c250c8bf6939e4a1999606401d96e4e058
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-14 15:05:56 +0900

    이미지 swticher 기능 완성

--- ImageSwitcher/app/src/main/java/kr/ac/tukorea/myapplication/imageswitcher2018182031/MainActivity.java
+++ ImageSwitcher/app/src/main/java/kr/ac/tukorea/myapplication/imageswitcher2018182031/MainActivity.java
@@ -6,21 +6,49 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private int pageIndex = 0;
+
+    private static final int[] IMG_RES_IDS = new int[]{
+            R.mipmap.cat_1,
+            R.mipmap.cat_2,
+            R.mipmap.cat_3,
+            R.mipmap.cat_4,
+            R.mipmap.cat_5,
+            R.mipmap.cat_6,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.imgView = findViewById(R.id.MainImageView);
+        this.tv = findViewById(R.id.pageTextView);
+        tv.setText((pageIndex+1) + " / " + MAX_IMG_COUNT);
     }
 
     public void onBtnPrev(View view) {
-        ImageView imgView = findViewById(R.id.MainSrcImg);
-        Log.d("MainActivity", "Prev Presssed");
+        setPage(pageIndex - 1);
+        Log.d(TAG, "Prev Presssed");
     }
 
     public void onBtnNext(View view) {
-        Log.d("MainActivity", "Next Presssed");
+        setPage(pageIndex + 1);
+        Log.d(TAG, "Next Presssed");
+    }
+    private ImageView imgView;
+    private TextView tv;
+    private final int MAX_IMG_COUNT = 6;
+
+    private void setPage(int index){
+        if(index < 0 || index > MAX_IMG_COUNT-1) return;
+        int resId = IMG_RES_IDS[index];
+        imgView.setImageResource(resId);
+        this.pageIndex = index;
+        tv.setText((pageIndex+1) + " / " + MAX_IMG_COUNT);
     }
 }
