617b17e2614548d8be737f2f57c8393f7c130dd9
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-20 15:57:31 +0900

    카드 뒷면을 각각 저장하여 표시

--- memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
+++ memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
@@ -13,6 +13,13 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private int[] resIds = new int[]{
+            R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,R.mipmap.card_5s,
+            R.mipmap.card_as,R.mipmap.card_jc,R.mipmap.card_kd,R.mipmap.card_qh,
+            R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,R.mipmap.card_5s,
+            R.mipmap.card_as,R.mipmap.card_jc,R.mipmap.card_kd,R.mipmap.card_qh,
+    };
+
     private static final int[] BUTTON_IDS = new int[]{
             R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
             R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
@@ -40,11 +47,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0;i< BUTTON_IDS.length;++i){
+            ImageButton btn = findViewById(BUTTON_IDS[i]);
+            btn.setTag(resIds[i]);
+        }
     }
 
+    private ImageButton previousButton;
     public void onBtnCard(View view){
         Log.d(TAG, "Card ID = " +getIndeWithId(view.getId()));
         ImageButton btn = (ImageButton) view;
         btn.setImageResource(R.mipmap.card_blue_back);
+        if(previousButton != null){
+            int resId = (Integer)previousButton.getTag();
+            previousButton.setImageResource(resId);
+        }
+        previousButton = btn;
     }
 }
