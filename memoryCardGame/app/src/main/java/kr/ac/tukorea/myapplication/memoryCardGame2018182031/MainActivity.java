9f6dc188fa657d77cd7088f9515b5d185dbc8bcb
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-20 14:37:09 +0900

    memory card 프로젝트 생성

---
+++ memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.myapplication.memoryCardGame2018182031;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
