ccfa7d83e73e797182160b5a9e73e1f09b8548dc
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-20 15:23:28 +0900

    id 인덱스

--- memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
+++ memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
@@ -8,9 +8,24 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int[] BUTTON_IDS = new int[]{
+            R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+            R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+            R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+            R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
+    };
+
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static int getIndeWithId(int id) {
+        for(int i =0; i<BUTTON_IDS.length; i++) {
+            if(BUTTON_IDS[i] == id){
+                return i;
+            }
+        }
+        return -1;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
