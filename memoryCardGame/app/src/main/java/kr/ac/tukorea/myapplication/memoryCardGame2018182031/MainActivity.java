56f593cb52ac36e98622aaa6e76211aef860874c
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-20 15:07:58 +0900

    onClick 함수, 태그, 로그 추가

--- memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
+++ memoryCardGame/app/src/main/java/kr/ac/tukorea/myapplication/memoryCardGame2018182031/MainActivity.java
@@ -3,12 +3,21 @@ package kr.ac.tukorea.myapplication.memoryCardGame2018182031;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnCard(View view){
+        Log.d(TAG, "Card ID = " + view.getId());
     }
 }
