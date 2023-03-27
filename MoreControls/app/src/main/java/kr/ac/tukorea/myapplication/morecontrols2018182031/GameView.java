fa8602559b8c80c500c8610ed0af478a2d878069
Lee Ji Hyeong <77761502+LeejH601@users.noreply.github.com>
2023-03-27 14:55:43 +0900

    스위치, 게임뷰, paint 한 번만 생성

---
+++ MoreControls/app/src/main/java/kr/ac/tukorea/myapplication/morecontrols2018182031/GameView.java
@@ -0,0 +1,61 @@
+package kr.ac.tukorea.myapplication.morecontrols2018182031;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+    private Paint paint;
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        Rect rect = new Rect(paddingLeft, paddingTop, getWidth() - paddingRight, getHeight() - paddingBottom);
+        canvas.drawRect(rect, paint);
+    }
+
+}
--- MoreControls/app/src/main/java/kr/ac/tukorea/myapplication/morecontrols2018182031/MainActivity.java
+++ MoreControls/app/src/main/java/kr/ac/tukorea/myapplication/morecontrols2018182031/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Switch;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView outputTextView;
     private TextView reactionTextView;
     private EditText nameEditText;
+    private Switch immediateSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,26 +30,36 @@ public class MainActivity extends AppCompatActivity {
         outputTextView = findViewById(R.id.outputTextView);
         reactionTextView = findViewById(R.id.reactionTextView);
         nameEditText = findViewById(R.id.nameEditText);
-        nameEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Log.v(TAG, "beforeTextChanged(i=" + i + ",i1=" + i1 + ",i2=" + "i2");
-            }
+        nameEditText.addTextChangedListener(textWatcher);
+        immediateSwitch = findViewById(R.id.immediateSwitch);
+    }
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            Log.v(TAG, "beforeTextChanged(i=" + i + ",i1=" + i1 + ",i2=" + "i2");
+        }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 Log.v(TAG, "onTextChanged(seq=" + charSequence + ",i=" + i + ",i1=" + i1 + ",i2=" + "i2");
+                outputTextView.setText("You entered " + charSequence.length() + " chars");
+                if (immediateSwitch.isChecked()) {
+                    doIt();
+                }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
                 Log.v(TAG, "afterTextChanged(" + editable.toString() + ")");
             }
-        });
-    }
+    };
 
     public void onBtnDoIt(View view) {
         Log.d(TAG, "Do It");
+        doIt(); // refactor - extract method
+    }
+
+    private void doIt() {
         int fmtResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good_fmt : R.string.pay_for_not_good_fmt;
         String name = nameEditText.getText().toString();
         if (name.trim().length() == 0) {
